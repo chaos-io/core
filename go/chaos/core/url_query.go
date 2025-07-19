2f9ba8ee816fa2da208d20268cf02b7c60c82ae3
Eric Yue <yuekewin@gmail.com>
2025-07-19 19:07:04 +0800

    feat: add url_query; add openapi json

---
+++ go/chaos/core/url_query.go
@@ -0,0 +1,94 @@
+package core
+
+import (
+	"net/url"
+	"strconv"
+)
+
+func NewUrlQuery(kvs ...any) *Query {
+	query := &Query{
+		Values: make(map[string]*StringValues),
+	}
+
+	for i := 0; i < len(kvs)-1; i += 2 {
+		key := kvs[i].(string)
+		query.Add(key, kvs[i+1])
+	}
+	return query
+}
+
+func NewUrlQueryFrom(values url.Values) *Query {
+	query := &Query{
+		Values: make(map[string]*StringValues),
+	}
+	return query.FromUrlValues(values)
+}
+
+func (x *Query) FromUrlValues(values url.Values) *Query {
+	if x != nil {
+		if x.Values == nil {
+			x.Values = make(map[string]*StringValues)
+		}
+
+		for k, v := range values {
+			x.Values[k] = &StringValues{Values: v}
+		}
+	}
+	return x
+}
+
+func (x *Query) Add(k string, v any) *Query {
+	if x != nil {
+		if x.Values[k] == nil {
+			x.Values[k] = &StringValues{}
+		}
+		x.Values[k].Values = append(x.Values[k].Values, queryValueFormat(v)...)
+	}
+	return x
+}
+
+func (x *Query) Set(k string, v any) *Query {
+	if x != nil {
+		x.Values[k] = &StringValues{
+			Values: queryValueFormat(v),
+		}
+	}
+	return x
+}
+
+func queryValueFormat(val any) []string {
+	switch v := val.(type) {
+	case bool:
+		return []string{strconv.FormatBool(v)}
+	case int8:
+		return []string{strconv.FormatInt(int64(v), 10)}
+	case int16:
+		return []string{strconv.FormatInt(int64(v), 10)}
+	case int32:
+		return []string{strconv.FormatInt(int64(v), 10)}
+	case int64:
+		return []string{strconv.FormatInt(v, 10)}
+	case int:
+		return []string{strconv.FormatInt(int64(v), 10)}
+	case uint8:
+		return []string{strconv.FormatUint(uint64(v), 10)}
+	case uint16:
+		return []string{strconv.FormatUint(uint64(v), 10)}
+	case uint32:
+		return []string{strconv.FormatUint(uint64(v), 10)}
+	case uint64:
+		return []string{strconv.FormatUint(v, 10)}
+	case uint:
+		return []string{strconv.FormatUint(uint64(v), 10)}
+	case float32:
+		return []string{strconv.FormatFloat(float64(v), 'g', -1, 32)}
+	case float64:
+		return []string{strconv.FormatFloat(v, 'g', -1, 64)}
+	case string:
+		return []string{v}
+	case []string:
+		return v
+	}
+
+	return []string{}
+}
